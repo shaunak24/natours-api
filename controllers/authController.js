db35a61b39afa3e96a79a4c14047f807ab55683a
Shaunak <shaunak241997@gmail.com>
2022-07-24 19:21:40 +0530

    Authorization: User role and permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,3 +77,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currectUser;
   next();
 });
+
+exports.restrict = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You are not authorized to access this resource'),
+        403
+      );
+    }
+    next();
+  };
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,10 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrict('admin', 'guide', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
