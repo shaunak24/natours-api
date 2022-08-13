ac27032bc9643fabb225dac0d20a645e2434bd70
Shaunak <shaunak241997@gmail.com>
2022-08-13 19:02:14 +0530

    Allow user to update data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,68 @@
-exports.getAllUsers = (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const User = require('../models/userModel');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+exports.getAllUsers = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.getUser = (req, res) => {
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // throw error if user tries to update password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'Unable to update password. Please use route /updateMyPassword'
+      )
+    );
+  }
+
+  // filter body
+  const newobj = filterObj(req.body, 'name', 'email');
+
+  // update user data
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, newobj, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
+exports.getUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.createUser = (req, res) => {
+exports.createUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.updateUser = (req, res) => {
+exports.updateUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.deleteUser = (req, res) => {
+exports.deleteUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,6 +14,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
