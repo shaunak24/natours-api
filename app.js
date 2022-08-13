c4d120d7dd19d39602dcd2962bc8f5de47f9a3c6
Shaunak <shaunak241997@gmail.com>
2022-08-13 23:31:05 +0530

    Data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // body parser, converts body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL injection attacks
+app.use(mongoSanitize());
+
+// data sanitization against XSS attacks
+app.use(xss());
 
 // serving static files
 app.use(express.static(`${__dirname}/public`));
